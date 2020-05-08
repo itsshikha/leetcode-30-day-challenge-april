5266dcec5e6cd5619a90e3dda278aff7be585cd7
itsshikha <39080296+itsshikha@users.noreply.github.com>
2020-05-08 10:17:08 +0530

    Create q8.cpp

---
+++ q8.cpp
@@ -0,0 +1,25 @@
+/*
+  Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+  If there are two middle nodes, return the second middle node.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode*slow = head, *fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
