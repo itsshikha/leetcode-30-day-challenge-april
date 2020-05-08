e514ee1d06ec3f6893fad2ab70d53d111e08a36a
itsshikha <39080296+itsshikha@users.noreply.github.com>
2020-05-08 10:41:07 +0530

    Create q26.cpp

---
+++ q26.cpp
@@ -0,0 +1,27 @@
+/*
+Given two strings text1 and text2, return the length of their longest common subsequence.
+
+A subsequence of a string is a new string generated from the original string with some characters(can be none) deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde" while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+If there is no common subsequence, return 0.
+
+ */
+ 
+ class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        for(int i=1; i<=text1.size(); i++){
+            for(int j=1; j<=text2.size(); j++){
+                if(text1[i-1] == text2[j-1])
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                else
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[text1.size()][text2.size()];
+        
+    }
+};
